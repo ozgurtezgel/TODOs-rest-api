1fbab7d1702905b63da862962cf62514ab4bbf3e
Ozgur Tezgel <ozgurtezgel11@gmail.com>
2022-04-10 20:05:06 +0200

    First commit

---
+++ src/main/java/com/example/TODOsRestApi/TodosRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TODOsRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TODOsRestApi/TodosRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TODOsRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
