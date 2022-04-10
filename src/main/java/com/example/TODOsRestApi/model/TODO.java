38809b976b4314aa971506212e0566bdac7bf45c
Ozgur Tezgel <ozgurtezgel11@gmail.com>
2022-04-10 20:15:54 +0200

    Create model class TODO

---
+++ src/main/java/com/example/TODOsRestApi/model/TODO.java
@@ -0,0 +1,36 @@
+package com.example.TODOsRestApi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TODO {
+
+    private Long id;
+    private Long userId;
+    @NotBlank(message = "Title can't be blank")
+    private String title;
+    private String due_on;
+    @NotBlank(message = "Status can't be blank")
+    @Pattern(regexp = "(completed|pending)", message = "Invalid input for status")
+    private String status;
+
+
+
+    @Override
+    public String toString() {
+        return "TODO{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", title='" + title + '\'' +
+                ", due_on=" + due_on +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
