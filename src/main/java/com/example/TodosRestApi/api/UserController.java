ff13c5a8ad12d70ea08e48482ad497f44f7cf930
Ozgur Tezgel <ozgurtezgel11@gmail.com>
2022-04-17 19:52:22 +0200

    Correct the formatting and mistakes

--- src/main/java/com/example/TodosRestApi/api/UserController.java
+++ src/main/java/com/example/TodosRestApi/api/UserController.java
@@ -68,20 +68,20 @@ public class UserController {
         throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid client id or secret");
     }
 
+//    @GetMapping(path = "{id}/todos")
+//    @ResponseStatus(HttpStatus.OK)
+//    public List<TODOItem> getTODOs(@PathVariable("id") Long id, @RequestParam(required = false, value = "title", defaultValue = "") String title, @RequestParam(required = false, value = "status", defaultValue = "") String status,
+//                                   @RequestHeader(value = "clientId") String clientId, @RequestHeader(value = "clientSecret") String clientSecret) {
+//
+//        if (clientId.equals(ID) && clientSecret.equals(secret)) {
+//            return userService.getTODOs(id, title, status);
+//        }
+//        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid client id or secret");
+//    }
+
     @GetMapping(path = "{id}/todos")
     @ResponseStatus(HttpStatus.OK)
-    public List<TODOItem> getTODOs(@PathVariable("id") Long id, @RequestParam(required = false, value = "title", defaultValue = "") String title, @RequestParam(required = false, value = "status", defaultValue = "") String status,
-                                   @RequestHeader(value = "clientId") String clientId, @RequestHeader(value = "clientSecret") String clientSecret) {
-
-        if (clientId.equals(ID) && clientSecret.equals(secret)) {
-            return userService.getTODOs(id, title, status);
-        }
-        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid client id or secret");
-    }
-
-    @GetMapping(path = "{id}/todoss")
-    @ResponseStatus(HttpStatus.OK)
-    public CollectionModel<TODOItem> getTODOss(@PathVariable("id") Long id, @RequestParam(required = false, value = "title", defaultValue = "") String title, @RequestParam(required = false, value = "status", defaultValue = "") String status,
+    public CollectionModel<TODOItem> getTODOs(@PathVariable("id") Long id, @RequestParam(required = false, value = "title", defaultValue = "") String title, @RequestParam(required = false, value = "status", defaultValue = "") String status,
                                    @RequestParam(required = false, value = "pageSize", defaultValue = "20") Integer pageSize, @RequestParam(required = false, value = "page", defaultValue = "0") Integer page,
                                    @RequestHeader(value = "clientId") String clientId, @RequestHeader(value = "clientSecret") String clientSecret) {
 
@@ -98,7 +98,7 @@ public class UserController {
                 Page<TODOItem> pageItem = new PageImpl<>(todos.subList(start, end), paging, todos.size());
                 result = CollectionModel.of(pageItem);
             } else {
-                Link nextLink = linkTo(methodOn(UserController.class).getTODOss(id, title, status, pageSize, page+1, clientId, clientSecret)).withRel("next");
+                Link nextLink = linkTo(methodOn(UserController.class).getTODOs(id, title, status, pageSize, page+1, clientId, clientSecret)).withRel("next");
                 Page<TODOItem> pageItem = new PageImpl<>(todos.subList(start, end), paging, todos.size());
                 result = CollectionModel.of(pageItem, nextLink);
             }
